2e34dbbaa246e0909f46b111f39c91d9d801fcfc
OneTeeApp <support@oneteeapp.com>
2026-08-08 14:56:43 -0600

    Mark Birch Creek golfrev experimental (browser-tier pending)

--- build_registry.py
+++ build_registry.py
@@ -680,7 +680,7 @@ IMPLEMENTED = {"foreup", "teeitup", "chronogolf", "clubprophet", "clubcaddie",
                "membersports", "quick18", "teesnap", "foretees",
                "golfwithaccess", "totale", "rguest", "courseco",
                "teequest", "clubessential", "resortsuite", "golfback",
-               "tenfore", "agilysys", "golfpay", "easytee", "golfrev"}
+               "tenfore", "agilysys", "golfpay", "easytee"}
 
 
 def slugify(name: str) -> str:
